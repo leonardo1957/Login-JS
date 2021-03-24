4e6086ec76a0dd2c7c925739c3da051361d6ccfa
Leonardo <l30v131r4@gmail.com>
2021-03-24 00:26:08 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render( <React.StrictMode >
     <App />
-    <p> teste </p> 
     </React.StrictMode>,
 
     document.getElementById('root')
